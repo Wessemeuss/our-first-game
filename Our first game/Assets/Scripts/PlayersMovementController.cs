950f60a6ba33b0a424e5440a457d47740c087768
Andrew Romanov <wessemeuss@mail.ru>
2018-08-13 21:37:54 +0400

    Добавлена базовая механика передвижения персонажа

---
+++ Our first game/Assets/Scripts/PlayersMovementController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersMovementController : MonoBehaviour {
+
+	public float PlayerSpeed = 200f; // Скорость персонажа
+	private Rigidbody2D _playerRigidbody2D; // Элемент Rigidbody2D нужен для обработки физики
+
+	void Start()
+	{
+		_playerRigidbody2D = GetComponent<Rigidbody2D>(); // Получаем компонент Rigidbody2D
+	}
+	
+	void FixedUpdate () 
+	{
+		var horizontal = Input.GetAxis("Horizontal"); // Получаем значение по оси X
+		var vertical = Input.GetAxis("Vertical"); // Получаем значение по оси Y
+		// Выставляем скорость объекта на основе значения осей, скорости и времени, прошедшего с последнего кадра
+		_playerRigidbody2D.velocity = new Vector2 (horizontal * PlayerSpeed * Time.deltaTime, vertical * PlayerSpeed * Time.deltaTime);
+	}
+}
